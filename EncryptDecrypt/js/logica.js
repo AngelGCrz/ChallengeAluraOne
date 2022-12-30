abc7fc3da752f4830b6a68f58df62b24a5b36d83
AngelGCrz <angelgcrz@gmail.com>
2022-12-29 23:03:25 -0500

    update index y logica
    se eliminaron algunos errores en el index, se agregaron funciones encriptar y desencriptar

--- EncryptDecrypt/js/logica.js
+++ EncryptDecrypt/js/logica.js
@@ -1,10 +1,41 @@
 
 //OCULTA CONTENIDO ETIQUEDA ASIDE
-function encriptar() {
+/*function encriptar() {
     let contenidoAside = document.getElementById('contenidoAside');
     if (contenidoAside.style.display === 'none') {
         contenidoAside.style.display = 'block';
-    }else{
+
+    } else {
         contenidoAside.style.display = 'none'
     }
+}*/
+
+function encriptar() {
+  // Obtener el texto de la caja de texto
+  let texto = document.getElementById("textoUsuario").value;
+
+  // Encriptar el texto
+  texto = texto.replace(/e/g, "enter");
+  texto = texto.replace(/i/g, "imes");
+  texto = texto.replace(/a/g, "ai");
+  texto = texto.replace(/o/g, "ober");
+  texto = texto.replace(/u/g, "ufat");
+
+  // Mostrar el texto encriptado en la caja de texto
+  document.getElementById("mostrarTexto").value = texto;
+}
+
+function desencriptar() {
+  // Obtener el texto de la caja de texto
+  let texto = document.getElementById("mostrarTexto").value;
+
+  // Desencriptar el texto
+  texto = texto.replace(/enter/g, "e");
+  texto = texto.replace(/imes/g, "i");
+  texto = texto.replace(/ai/g, "a");
+  texto = texto.replace(/ober/g, "o");
+  texto = texto.replace(/ufat/g, "u");
+
+  // Mostrar el texto desencriptado en la caja de texto
+  document.getElementById("mostrarTexto").value = texto;
 }
